58757190140b2b65b66637161ab7736f2cbd704e
Laurent Brugiere <laurentbrugiere@yahoo.fr>
2023-12-20 06:29:52 +0000

    Rollbar causing backend server to crash

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -93,36 +93,37 @@ def rollbar_test():
     rollbar.report_message('Hello World!', 'warning')
     return "Hello World!"
 
-## XXX hack to make request data work with pyrollbar <= 0.16.3
-def _get_flask_request():
-    print("Getting flask request")
-    from flask import request
-    print("request:", request)
-    return request
-rollbar._get_flask_request = _get_flask_request
+# ## XXX hack to make request data work with pyrollbar <= 0.16.3
+# def _get_flask_request():
+#     print("Getting flask request")
+#     from flask import request
+#     print("request:", request)
+#     return request
+# rollbar._get_flask_request = _get_flask_request
 
-def _build_request_data(request):
-    return rollbar._build_werkzeug_request_data(request)
-rollbar._build_request_data = _build_request_data
-## XXX end hack
+# def _build_request_data(request):
+#     return rollbar._build_werkzeug_request_data(request)
+# rollbar._build_request_data = _build_request_data
+# ## XXX end hack
 
-# Rollbar Initialisation
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        'production',
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
+# Getting an error here: 'Flask' object has no attribute 'before_first_request'. Did you mean: '_got_first_request'?
+# # Rollbar Initialisation
+# rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+# @app.before_first_request
+# def init_rollbar():
+#     """init rollbar module"""
+#     rollbar.init(
+#         # access token
+#         rollbar_access_token,
+#         # environment name
+#         'production',
+#         # server root directory, makes tracebacks prettier
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         # flask already sets up logging
+#         allow_logging_basic_config=False)
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
