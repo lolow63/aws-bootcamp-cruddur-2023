66d625e825b97bb856b74ec16a99801a3916e9cf
Laurent Brugiere <laurentbrugiere@yahoo.fr>
2023-12-20 02:36:46 +0000

    add segmentsd and subsegment

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,10 +1,15 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
+    # Start a segment
+    segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
     }
+    # Start a subsegment
+    subsegment = xray_recorder.begin_subsegment('mock-data')
 
     now = datetime.now(timezone.utc).astimezone()
 
@@ -20,4 +25,16 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+
+    # X-ray
+    # Add metadata or annotation here if necessary
+    dict= {
+      "now": now.isoformat(),
+      "result-size": len(model['data'])
+    }
+    segment.put_metadata('key', dict, 'namespace'
+
+    # Add metadata or annotation here if necessary
+    segment.put_metadata('key', dict, 'namespace')
+
     return model
